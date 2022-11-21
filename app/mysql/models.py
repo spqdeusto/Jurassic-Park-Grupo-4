0db26aed9dca7a471f4cc501d96745389e488de0
Iker Galarza <iker.galarza@opendeusto.es>
2022-11-21 15:48:01 +0100

    dinosaurio

--- app/mysql/models.py
+++ app/mysql/models.py
@@ -1,18 +1,40 @@
 from sqlalchemy import Column, Integer, String, Sequence
 from app.mysql.base import Base
+from sqlalchemy import Enum
 
+class Species(enum.Enum):
+  dilophosaurus = 'dilophosaurus'
+  trex = 't-rex'
+  velociraptor = 'velociraptor'
+  brachiosaurus = 'brachiosaurus'
+  parasaulophus = 'parasaulophus'
+  galliminus = 'galliminus'
+  triceratops = 'triceratops'
+class Gender(enum.Enum):
+  male = 'male'
+  female = 'female'
 
-class User(Base):
-  __tablename__ = "users"
-  id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
+class Dangerousness(enum.Enum):
+  peaceful = 'peaceful'
+  aggressive = 'aggressive'
+
+class Dinosaur(Base):
+  __tablename__ = "dinosaurs"
+  id = Column(Integer, Sequence("dino_id_seq"), primary_key=True)
   name = Column(String(50))
-  fullname = Column(String(50))
+  species = Column(Enum(Species))
   age = Column(Integer)
+  weigth = Column(Integer)
+  gender = Column(Enum(Gender))
+  dangerousness = Column(Enum(Dangerousness))
 
   def __repr__(self) -> str:
-    return "<User(id= '%d', name='%s', fullname='%s', age='%d')>" % (
+    return "<(id= '%d', name='%s', specie='%s', age='%d', weigth='%d', gender='%s', dangerousness='%s')>" % (
       self.id,
       self.name,
-      self.fullname,
+      self.species,
       self.age,
+      self.weigth,
+      self.gender,
+      self.dangerousness
     )
