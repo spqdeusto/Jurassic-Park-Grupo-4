5de8f7041834e8a7d2dff6ae093afe67ca13fc70
unaiarambarri <unai.arambarri@opendeusto.es>
2022-12-15 16:21:51 +0100

    Get, create and update methods for all objects

--- app/controllers/handler.py
+++ app/controllers/handler.py
@@ -10,12 +10,10 @@ class Controllers:
   def __init__(self) -> None:
     pass
   
-  def create_enclosure(self, name, status):
-    """
-    Creates new user in  the database
-    """
-    body_row = mysql_models.Enclosure(name=name, status=status)
-    
+  # CREATE METHODS 
+
+  def create_dinosaur(self, body: models.Dinosaur):
+    body_row = mysql_models.Dinosaur(name=body.name, specie_id=body.specie_id, age=body.age, weight=body.weight, gender=body.gender, dangerousness=body.dangerousness, enclosure_id=body.encosure_id)
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
       session.add(body_row)
@@ -24,31 +22,8 @@ class Controllers:
   
     return {"status": "ok"}
 
-  def create_specie(self):
-    body_row = mysql_models.Dinosaur(name="T-Rex")
-    db = DatabaseClient(gb.MYSQL_URL)
-    with Session(db.engine) as session:
-      session.add(body_row)
-      session.commit()
-      session.close()
-
-
-  def create_dinosaur(self):
-    body_row = mysql_models.Dinosaur(name="Toby", species=1, age= 16, weight = 100, gender = "male", dangerousness = "peaceful", enclosure_id = 1)
-    db = DatabaseClient(gb.MYSQL_URL)
-    with Session(db.engine) as session:
-      session.add(body_row)
-      session.commit()
-      session.close()
-  
-    return {"status": "ok"}
-  
-  def create_offroad(self):
-    """
-    Creates new user in  the database
-    """
-    body_row = mysql_models.OffRoad(on_route = True, n_visitors = 100, security_system= False)
-    
+  def create_specie(self, body: models.Specie):
+    body_row = mysql_models.Species(name=body.name)
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
       session.add(body_row)
@@ -57,12 +32,8 @@ class Controllers:
   
     return {"status": "ok"}
 
-  def create_alarm(self):
-    """
-    Creates new user in  the database
-    """
-    body_row = mysql_models.Alarm(status = "maximum")
-    
+  def create_enclosure(self, body: models.Enclosure):
+    body_row = mysql_models.Enclosure(name=body.name, status=body.status)
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
       session.add(body_row)
@@ -71,40 +42,8 @@ class Controllers:
   
     return {"status": "ok"}
 
-    #-------
-  
-  def create_specieModel(self, body: models.Specie):
-    body_row = mysql_models.Species(name = body.name)
-    db = DatabaseClient(gb.MYSQL_URL)
-    with Session(db.engine) as session:
-      session.add(body_row)
-      session.commit()
-      session.close()
-  
-    return {"status": "ok"}
-
-  def create_enclosureModel(self, body: models.Enclosure):
-    body_row = mysql_models.Enclosure(name = body.name, status = body.status)
-    db = DatabaseClient(gb.MYSQL_URL)
-    with Session(db.engine) as session:
-      session.add(body_row)
-      session.commit()
-      session.close()
-  
-    return {"status": "ok"}
-
-  def create_dinosaurModel(self, body: models.Dinosaur):
-    body_row = mysql_models.Dinosaur(name=body.name, specie_id=body.specie_id, age= body.weight, weight = body.weight, gender = body.gender, dangerousness = body.dangerousness, enclosure_id = body.encosure_id)
-    db = DatabaseClient(gb.MYSQL_URL)
-    with Session(db.engine) as session:
-      session.add(body_row)
-      session.commit()
-      session.close()
-  
-    return {"status": "ok"}
-
-  def create_offRoadModel(self, body: models.OffRoad):
-    body_row = mysql_models.OffRoad(on_route = body.on_route, n_visitors = body.n_visitors, security_system = body.security_system)
+  def create_offroad(self, body: models.OffRoad):
+    body_row = mysql_models.OffRoad(on_route=body.on_route, n_visitors=body.n_visitors, security_system=body.security_system)
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
       session.add(body_row)
@@ -113,8 +52,8 @@ class Controllers:
 
     return {"status": "ok"}
 
-  def create_alarmModel(self, body: models.Alarm):
-    body_row = mysql_models.Alarm(status = body.status)
+  def create_alarm(self, body: models.Alarm):
+    body_row = mysql_models.Alarm(status=body.status)
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
       session.add(body_row)
@@ -123,7 +62,9 @@ class Controllers:
 
     return {"status": "ok"}
 
-  def get_Dinosaurs(self):
+  # GET METHODS
+
+  def get_dinosaurs(self):
     db = DatabaseClient(gb.MYSQL_URL)
     response: list = []
     with Session(db.engine) as session:
@@ -132,7 +73,16 @@ class Controllers:
       
     return response
 
-  def get_Enclosures(self):
+  def get_species(self):
+    db = DatabaseClient(gb.MYSQL_URL)
+    response: list = []
+    with Session(db.engine) as session:
+      response = session.query(mysql_models.Specie).all()
+      session.close()
+      
+    return response
+
+  def get_enclosures(self):
     db = DatabaseClient(gb.MYSQL_URL)
     response: list = []
     with Session(db.engine) as session:
@@ -141,7 +91,7 @@ class Controllers:
       
     return response
 
-  def get_OffRoads(self):
+  def get_offroads(self):
     db = DatabaseClient(gb.MYSQL_URL)
     response: list = []
     with Session(db.engine) as session:
@@ -150,7 +100,7 @@ class Controllers:
       
     return response
   
-  def get_Alarms(self):
+  def get_alarms(self):
     db = DatabaseClient(gb.MYSQL_URL)
     response: list = []
     with Session(db.engine) as session:
@@ -159,6 +109,73 @@ class Controllers:
       
     return response
 
+  # UPDATE METHODS 
+
+  def update_dinosaur(self, dinosaur_id, body: models.Dinosaur):
+    body_row = mysql_models.Dinosaur(name=body.name, specie_id=body.specie_id, age=body.age, weight=body.weight, gender=body.gender, dangerousness=body.dangerousness, enclosure_id=body.encosure_id)
+    db = DatabaseClient(gb.MYSQL_URL)
+    with Session(db.engine) as session:
+      dinosaur: mysql_models.Dinosaur = session.query(mysql_models.Dinosaur).get(dinosaur_id)
+      dinosaur.name = body.name
+      dinosaur.specie_id = body.specie_id
+      dinosaur.age = body.age
+      dinosaur.weight = body.weight
+      dinosaur.gender = body.gender
+      dinosaur.dangerousness = body.dangerousness
+      dinosaur.encosure_id = body.encosure_id
+      session.dirty
+      session.commit()
+      session.close()
   
+    return {"status": "ok"}
+    
+
+  def update_specie(self, specie_id, body: models.Specie):
+    body_row = mysql_models.Species(name = body.name)
+    db = DatabaseClient(gb.MYSQL_URL)
+    with Session(db.engine) as session:
+      specie: mysql_models.Specie = session.query(mysql_models.Specie).get(specie_id)
+      specie.name = body.name
+      session.dirty
+      session.commit()
+      session.close()
+  
+    return {"status": "ok"}
+
+  def update_enclosure(self, enclosue_id, body: models.Enclosure):
+    body_row = mysql_models.Enclosure(name = body.name, status = body.status)
+    db = DatabaseClient(gb.MYSQL_URL)
+    with Session(db.engine) as session:
+      enclosure: mysql_models.Enclosure = session.query(mysql_models.Enclosure).get(enclosue_id)
+      enclosure.name = body.name
+      enclosure.status = body.status
+      session.commit()
+      session.close()
+  
+    return {"status": "ok"}
+
+  def update_offroad(self, offroad_id, body: models.OffRoad):
+    body_row = mysql_models.OffRoad(on_route = body.on_route, n_visitors = body.n_visitors, security_system = body.security_system)
+    db = DatabaseClient(gb.MYSQL_URL)
+    with Session(db.engine) as session:
+      offroad: mysql_models.OffRoad = session.query(mysql_models.OffRoad).get(offroad_id)
+      offroad.on_route = body.on_route
+      offroad.n_visitors = body.n_visitors
+      offroad.security_system = body.security_system
+      session.commit()
+      session.close()
+
+    return {"status": "ok"}
+
+  def update_alarm(self, alarm_id, body: models.Alarm):
+    body_row = mysql_models.Alarm(status = body.status)
+    db = DatabaseClient(gb.MYSQL_URL)
+    with Session(db.engine) as session:
+      alarm: mysql_models.Alarm = session.query(mysql_models.Alarm).get(alarm_id)
+      alarm.status = body.status
+      session.commit()
+      session.close()
+
+    return {"status": "ok"}
 
   
